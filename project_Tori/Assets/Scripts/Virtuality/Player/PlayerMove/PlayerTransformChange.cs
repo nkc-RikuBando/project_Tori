5dbf89d27fdd09f3968f8faf842e264ab485a781
DESKTOP-E8EHEE1\user <breakretstoda@ezweb.ne.jp>
2021-11-04 17:37:35 +0900

    Add テストプレイなんてしてないよ

--- project_Tori/Assets/Scripts/Virtuality/Player/PlayerMove/PlayerTransformChange.cs
+++ project_Tori/Assets/Scripts/Virtuality/Player/PlayerMove/PlayerTransformChange.cs
@@ -19,34 +19,12 @@ namespace Virtuality
             Subject<PlayerMoveType> _playerMoveTypeSubject = new Subject<PlayerMoveType>();
 
             private Vector3    _currentVelocity;
-
-<<<<<<< HEAD
             private const float _PLAYER_SPEED = 15f;
-=======
-            private float _playerRotateSpeed_Y = 0f;
-            private const float _PLAYER_SPEED = 50f;
-            private const float _PLAYER_ACCEL_RATE = 0.5f;
-            private const float _ROTATE_SPEED_CHANGE_RATE = 0.1f;
-
-
-            // 一つ前のvelocityの値を取得するインターフェース
-            void IInputRecevable.OldMoveDir(Vector3 vec)
-            {
-                _oldVelocity = vec;
-            }
->>>>>>> c7c9012343726ae0ce3e59100d64c322e44949e9
-
-            //private float _playerRotateSpeed_Y = 0f;
-            //private const float _ROTATE_SPEED_CHANGE_RATE = 0.01f;
             
             // 移動用インターフェース実装
             void IInputRecevable.CurrentMoveDir(Vector3 moveDir, Vector3 oldVelo)
             {
-<<<<<<< HEAD
                 _currentVelocity = Vector3.Lerp(oldVelo, moveDir.normalized * _PLAYER_SPEED, Time.deltaTime);
-=======
-                _currentVelocity = Vector3.Lerp(_oldVelocity, vec * _PLAYER_SPEED, _PLAYER_ACCEL_RATE*Time.deltaTime);
->>>>>>> c7c9012343726ae0ce3e59100d64c322e44949e9
 
                 _playerVec3PosSubject.OnNext(_currentVelocity);
             }
@@ -54,29 +32,6 @@ namespace Virtuality
             // 回転用インターフェース実装
             void IInputRecevable.CurrentRatateDir(Vector3 moveDir, GameObject obj)
             {
-                {
-                //_playerRotateSpeed_Y += _ROTATE_SPEED_CHANGE_RATE * inputVec.x;
-
-                //// Y軸の回転　もっと綺麗になれるよお前は。
-                //if (inputVec.x == 0)
-                //{
-                //    if (_playerRotateSpeed_Y > 0)
-                //    {
-                //        _playerRotateSpeed_Y -= _ROTATE_SPEED_CHANGE_RATE;
-                //    }
-                //    else if (_playerRotateSpeed_Y < 0)
-                //    {
-                //        _playerRotateSpeed_Y += _ROTATE_SPEED_CHANGE_RATE;
-                //    }
-                //}
-
-                //_playerRotateSpeed_Y = Mathf.Clamp(_playerRotateSpeed_Y, -1f, 1f);
-
-                //_currentRot.y = _playerRotateSpeed_Y;
-
-
-                }
-
                 if (moveDir.magnitude > 0.1f)
                 {
                     Quaternion lookRotation = Quaternion.LookRotation(moveDir);
@@ -123,55 +78,6 @@ namespace Virtuality
                     if (velocity.y >= 0) _playerMoveTypeSubject.OnNext(FLIGHT_STRAIGHT);
                     else _playerMoveTypeSubject.OnNext(GLIDE_STRAIGHT);
                 }
-
-
-                //// 動いてない時
-                //if (velocity.magnitude <= 0.2f)
-                //{
-                //    _playerMoveTypeSubject.OnNext(HOVER);
-                //    return;
-                //}
-
-                //// 上昇状態
-                //if (velocity.y >= 0)
-                //{
-                //    if(inputVec.z == 0)
-                //    {
-                //        _playerMoveTypeSubject.OnNext(HOVER);
-                //    }
-                //    else if (inputVec.x > 0)
-                //    {
-                //        _playerMoveTypeSubject.OnNext(FLIGHT_RIGHT);
-                //    }
-                //    else if (inputVec.x < 0)
-                //    {
-                //        _playerMoveTypeSubject.OnNext(FLIGHT_LEFT);
-                //    }
-                //    else
-                //    {
-                //        _playerMoveTypeSubject.OnNext(FLIGHT_STRAIGHT);
-                //    }
-                //}
-                //// 降下状態
-                //else
-                //{
-                //    if(inputVec.z == 0)
-                //    {
-                //        _playerMoveTypeSubject.OnNext(HOVER);
-                //    }
-                //    else if (inputVec.x > 0)
-                //    {
-                //        _playerMoveTypeSubject.OnNext(GLIDE_RIGHT);
-                //    }
-                //    else if (inputVec.x < 0)
-                //    {
-                //        _playerMoveTypeSubject.OnNext(GLIDE_LEFT);
-                //    }
-                //    else
-                //    {
-                //        _playerMoveTypeSubject.OnNext(GLIDE_STRAIGHT);
-                //    }
-                //}
 
             }
         }
